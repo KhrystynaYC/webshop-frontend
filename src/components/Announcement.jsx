a35faaf0d7ef6071be73dd00d0cf6d7b3875dd6b
Khrystyna Leshko <k.leshko@youngcapital.nl>
2023-08-09 13:06:05 +0200

    uploading front end

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { styled } from 'styled-components'
+
+const Container = styled.div`
+  height: 30px;
+  background-color: #f1ebf3;
+  color: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-style: 14px;
+  font-weight: 500;
+`
+const Announcement = () => {
+  return (
+    <Container>
+      Super Deal! Free Shipping on orders Over $50!
+    </Container>
+  )
+}
+
+export default Announcement
---
+++ src/data.js
@@ -0,0 +1,92 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "/src/assets/slider1-homepage1.jpg",
+    title: "TRENDING NOW",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "ebeff0",
+  },
+  {
+    id: 2,
+    img: "/src/assets/slider2-homepage.jpg",
+    title: "EASY ELEGANCE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 40% OFF FOR NEW ARRIVALS.",
+    bg: "fafafa",
+  },
+  {
+    id: 3,
+    img: "/src/assets/slider3-homepage.jpg",
+    title: "ACCESSOIRES EDIT",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 50% OFF FOR NEW ARRIVALS.",
+    bg: "e9e9eb",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "/src/assets/yellow-bg.jpg",
+    title: "ENDLESS SUMMER",
+  },
+  {
+    id: 2,
+    img: "/src/assets/flowers-dress.jpg",
+    title: "DRESS SELECTION",
+  },
+  {
+    id: 3,
+    img: "/src/assets/man-shirt.jpg",
+    title: "ELEGANT UTILITY",
+  },
+];
+
+export const popularProducts = [
+  {
+    id:1,
+    img:"/src/assets/pink-sweater-front-removebg.png",
+  },
+  {
+    id:2,
+    img:"/src/assets/black-front-sweater-removebg.png",
+  },
+  {
+    id:3,
+    img:"/src/assets/green-front-sweater-removebg.png",
+  },
+  {
+    id:4,
+    img:"/src/assets/pink-sweater-front-removebg.png",
+  },
+  {
+    id:5,
+    img:"/src/assets/black-front-sweater-removebg.png",
+  },
+  {
+    id:6,
+    img:"/src/assets/green-front-sweater-removebg.png",
+  },
+  {
+    id:7,
+    img:"/src/assets/pink-sweater-front-removebg.png",
+  },
+  {
+    id:8,
+    img:"/src/assets/black-front-sweater-removebg.png",
+  },
+  {
+    id:9,
+    img:"/src/assets/green-front-sweater-removebg.png",
+  },
+  {
+    id:10,
+    img:"/src/assets/pink-sweater-front-removebg.png",
+  },
+  {
+    id:11,
+    img:"/src/assets/black-front-sweater-removebg.png",
+  },
+  {
+    id:12,
+    img:"/src/assets/green-front-sweater-removebg.png",
+  },
+]
